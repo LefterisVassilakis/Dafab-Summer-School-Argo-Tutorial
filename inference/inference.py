6d8e2000018aa8123beb97e030d3ffc6d8bb1e46
Lefteris Vassilakis <csd4401@csd.uoc.gr>
2025-09-19 13:28:55 +0300

    Add threshold parameter for inference

--- inference/inference.py
+++ inference/inference.py
@@ -3,7 +3,8 @@ import numpy as np
 import sys
 
 input_file = sys.argv[1]
-output_file = sys.argv[2]
+threshold = sys.argv[2]
+output_file = sys.argv[3]
 
 # Open NVDI file
 with rasterio.open(input_file) as src:
@@ -11,7 +12,8 @@ with rasterio.open(input_file) as src:
     profile = src.profile
 
 # Compute mask
-classified = np.where(ndvi > 0.3, 1, 0)
+threshold = float(threshold)
+classified = np.where(ndvi > threshold, 1, 0)
 
 # Save make GeoTiff
 profile.update(dtype=rasterio.uint8, count=1)
